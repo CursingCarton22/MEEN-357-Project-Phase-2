22232f2ad8891eb7ff36ca3545c4b85ea1d67c57
ahrenpeters <ap106463@tamu.edu>
2026-03-11 13:45:47 -0500

    Add files via upload

---
+++ subfunctions.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+def end_of_mission_event(end_event):
+    """
+    Defines an event that terminates the mission simulation. Mission is over
+    when rover reaches a certain distance, has moved for a maximum simulation 
+    time or has reached a minimum velocity.            
+    """
+    
+    mission_distance = end_event['max_distance']
+    mission_max_time = end_event['max_time']
+    mission_min_velocity = end_event['min_velocity']
+    
+    # Assume that y[1] is the distance traveled
+    distance_left = lambda t,y: mission_distance - y[1]
+    distance_left.terminal = True
+    
+    time_left = lambda t,y: mission_max_time - t
+    time_left.terminal = True
+    
+    velocity_threshold = lambda t,y: y[0] - mission_min_velocity;
+    velocity_threshold.terminal = True
+    velocity_threshold.direction = -1
+    
+    # terminal indicates whether any of the conditions can lead to the
+    # termination of the ODE solver. In this case all conditions can terminate
+    # the simulation independently.
+    
+    # direction indicates whether the direction along which the different
+    # conditions is reached matters or does not matter. In this case, only
+    # the direction in which the velocity treshold is arrived at matters
+    # (negative)
+    
+    events = [distance_left, time_left, velocity_threshold]
+    
+    return events
+
+def experiment1():
+    
+    experiment = {'time_range' : np.array([0,20000]),
+                  'initial_conditions' : np.array([0.325,0]),
+                  'alpha_dist' : np.array([0, 100, 200, 300, 400, 500, 600, \
+                                           700, 800, 900, 1000]),
+                  'alpha_deg' : np.array([2.032, 11.509, 2.478, 7.182, \
+                                        5.511, 10.981, 5.601, -0.184, \
+                                        0.714, 4.151, 4.042]),
+                  'Crr' : 0.1}
+    
+    
+    # Below are default values for example only:
+    end_event = {'max_distance' : 50,
+                 'max_time' : 5000,
+                 'min_velocity' : 0.01}
+    
+    return experiment, end_event
