7bf69134183aece28bcc7df0e9899006d28e57de
ahrenpeters <ap106463@tamu.edu>
2026-03-15 14:12:20 -0500

    added battenergy

--- subfunctions.py
+++ subfunctions.py
@@ -456,3 +456,45 @@ def mechpower(v, rover):
     
     return Power
 
+
+from subfunctions import *
+
+
+# Function
+def battenergy(t, v, rover):
+    
+    # Imports
+    import numpy as np
+    
+    
+    # Checking the validity of inputs
+    if len(t) != len(v):
+        raise Exception("t and v should be equal length vectors")
+    if isinstance(rover, dict):
+        raise Exception("rover should be a dictionary")
+    
+    # Calling motorW to get angular speed
+    omega = motorW(v, rover)
+    
+    # Get motor information from rover dictionary
+    motor = rover['wheel_assembly']['motor']
+    
+    # Plug into functions to get tau and power
+    tau = tau_dcmotor(omega, motor)
+    mech_power = mechpower(v, rover)
+    
+    # Efficiency as a function of torque
+    tau_efficiency = rover['wheel_assembly']['motor']['effcy_tau']
+    efficiency = rover['wheel_assembly']['motor']['effcy']
+    
+    # Interpolate values 
+    eff_interpolate = interp1d(tau_efficiency, efficiency, fill_value = 'extrapolate')
+    final_efficiency = eff_interpolate(tau)
+    
+    # Calculating Power
+    power = 6 * mech_power / final_efficiency
+    
+    # Integrating using trapezoidal method to get electrical energy consumed
+    E = np.trapz(power, t)
+    
+    return E
